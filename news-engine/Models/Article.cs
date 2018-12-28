dcf0182f8f30034fdd3f84d0357f0428a77a42e8
Stefan Nita <stefannita01@gmail.com>
2018-12-28 02:32:29 +0200

    add Article model

---
+++ news-engine/Models/Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace news_engine.Models
+{
+    public class Article
+    {
+        [Key]
+        public int ArticleId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+
+    public class ArticleDbContext : DbContext
+    {
+        public ArticleDbContext() : base("DbConnection") { }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
