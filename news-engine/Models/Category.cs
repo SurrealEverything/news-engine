ad4a734140ef0ee4e38dc9a21fd9227578e33836
Dumitrescu Gabriel Horia <33297592+SurrealEverything@users.noreply.github.com>
2018-12-28 02:40:43 +0200

    category added

---
+++ news-engine/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace news_engine.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+    }
+
+    public class CategoryDbContext : DbContext
+    {
+        public CategoryDbContext() : base("DbConnection") { }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
